725a851b774955273a7d6cf89d577e859475aa45
Javier Gomez <javiergomezve@gmail.com>
2019-11-16 10:09:19 -0500

    Basic tut of RxJs

---
+++ src/Auth/config.js
@@ -0,0 +1,5 @@
+export const config = {
+    clientID: process.env.REACT_APP_CLIENT_ID,
+    domain: process.env.REACT_APP_DOMAIN,
+    redirectUri: process.env.REACT_APP_REDIRECTURI
+};
---
+++ src/Components/SecuredRoute/SecuredRoute.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, withRouter } from 'react-router-dom';
+
+import Auth0 from '../../Auth/Auth';
+
+const SecuredRoute = (props) => {
+    const { component: Component, path, checkingSession } = props;
+
+    return (
+        <Route
+            path={path}
+            render={() => {
+                if (checkingSession) return <button onClick={Auth0.signIn} className="py-6 px-8 text-3xl rounded shadow bg-yellow-lighter hover:bg-yellow-lightest text-yellow-darker">Log In Again</button>;
+
+                if (!Auth0.isAuthenticated()) {
+                    Auth0.signIn();
+                    return <div/>;
+                }
+
+                return <Component />
+            }}
+        />
+    );
+};
+
+export default withRouter(SecuredRoute);
